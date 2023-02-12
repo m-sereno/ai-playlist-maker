78c89d555e6ca5ec25de5f4470b76437190602c3
Mateus Sereno <mateus.sereno@gmail.com>
2023-02-12 14:51:11 -0300

    debugging (server-side)

--- functions/_getTracksFromDescription.js
+++ functions/_getTracksFromDescription.js
@@ -2,13 +2,19 @@ import generateTextCompletion from "./_generateTextCompletion";
 import getMusicInformation from "./_getMusicInformation";
 
 export default async function getTracksFromDescription(description) {
+  console.debug(`\nDescription:\n ${description}`);
 
   const textCompletion = await generateTextCompletion(description);
+  console.debug(`Text Completion: ${textCompletion}`);
+
   const songStrings = textCompletion.split('\n');
   var tracks = [];
 
-  await Promise.all(songStrings.map(async (trackStr) => {
+  await Promise.all(songStrings.map(async (trackStr, index) => {
     try {
+      if (index === 0 && trackStr == "") {
+        return;
+      }
       const trackInformation = await getMusicInformation(trackStr);
       tracks.push(trackInformation);
     } catch (error) {
@@ -16,5 +22,7 @@ export default async function getTracksFromDescription(description) {
     }
   }));
 
+  console.debug('Track Information:');
+  console.table(tracks, ['trackName', 'artist']);
   return tracks;
 }
