119a986263885cd3d7541a69dc0bb3eb1f7ed386
Mateus Sereno <mateus.sereno@gmail.com>
2023-02-12 01:20:46 -0300

    Player styling

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import { IconButton, useTheme } from "@mui/material";
 
 const useAudio = url => {
   const [audio] = useState(new Audio(url));
@@ -7,8 +10,8 @@ const useAudio = url => {
   const toggle = () => setPlaying(!playing);
 
   useEffect(() => {
-      playing ? audio.play() : audio.pause();
-    },
+    playing ? audio.play() : audio.pause();
+  },
     [playing]
   );
 
@@ -24,10 +27,13 @@ const useAudio = url => {
 
 const AudioPlayer = ({ url }) => {
   const [playing, toggle] = useAudio(url);
+  const theme = useTheme();
 
   return (
     <div>
-      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+      <IconButton onClick={toggle} color="primary">
+        {playing ? <PauseCircleIcon /> : <PlayCircleIcon />}
+      </IconButton>
     </div>
   );
 };
--- components/TrackView.js
+++ components/TrackView.js
@@ -5,11 +5,11 @@ export default function TrackView({ trackInfo }) {
   const theme = useTheme();
 
   return (
-    <Box display="flex" flexDirection="row" alignItems="center" sx={{ m: 2 }}>
+    <Box display="flex" flexDirection="row" alignItems="center" sx={{ m: 1 }}>
       <img src={trackInfo.albumCover} width={80} />
-      <Box display="flex" flexDirection="column" sx={{ m: 1 }}>
+      <Box display="flex" flexDirection="column" sx={{ ml: 1 }}>
         <Typography variant="h6">{trackInfo.trackName}</Typography>
-        <Typography variant="body2" color={theme.palette.grey[400]}>{JSON.stringify(trackInfo.artist)}</Typography>
+        <Typography variant="body2" color={theme.palette.grey[400]}>{trackInfo.artist}</Typography>
         <AudioPlayer url={trackInfo.audioPreviewUrl} />
       </Box>
     </Box>
