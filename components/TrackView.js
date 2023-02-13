a35d41384cd99afdeb74af8c9c24d3a6c9cae814
Mateus Sereno <mateus.sereno@gmail.com>
2023-02-13 09:15:41 -0300

    Tooltip styling

--- components/TrackView.js
+++ components/TrackView.js
@@ -1,4 +1,4 @@
-import { Box, Skeleton, Tooltip, Typography, useTheme } from "@mui/material";
+import { Box, Skeleton, styled, Tooltip, tooltipClasses, Typography, useTheme } from "@mui/material";
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import InfoIcon from '@mui/icons-material/Info';
 import AudioPlayer from "./AudioPlayer";
@@ -22,7 +22,15 @@ const AlbumCover = (props) => (
     ) : null}
     {props.children}
   </Box>
-)
+);
+
+const CustomWidthTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    maxWidth: 500,
+  },
+});
 
 export default function TrackView({ trackInfo }) {
   const theme = useTheme();
@@ -48,15 +56,17 @@ export default function TrackView({ trackInfo }) {
           <Typography variant="body2" color={theme.palette.grey[400]}>{trackInfo.artist}</Typography>
           <AudioPlayer key={trackInfo.audioPreviewUrl} url={trackInfo.audioPreviewUrl} />
         </TitleArtistPlayerBox>
-        <Tooltip placement="left-start" arrow title={trackInfo.explanation}>
+        <CustomWidthTooltip placement="right-start" sx={{ maxWidth: 800 }}
+          title={<Typography variant="body2" whiteSpace="pre-line"  >{trackInfo.explanation}</Typography>}
+        >
           <InfoIcon sx={{ mb: "auto", mt: 1, ml: 1 }} />
-        </Tooltip>
+        </CustomWidthTooltip>
       </TrackViewBox>
     );
   }
 
   return (
-    <Tooltip placement="right" title="This song couldn't be found on Spotify. Maybe it doesn't exist.">
+    <CustomWidthTooltip placement="right" title={<Typography variant="body2" whiteSpace="pre-line">{`This song couldn't be found on Spotify.\nMaybe it doesn't exist.`}</Typography>}>
       <Box width="max-content" height={100} display="flex" flexDirection="row" alignItems="center" sx={{ m: .5 }}>
         <Box display="flex" justifyContent="center" alignItems="center" sx={{ width: 80, height: 80 }}>
           <QuestionMarkIcon fontSize="large" />
@@ -66,6 +76,6 @@ export default function TrackView({ trackInfo }) {
           <Typography variant="body2" color={theme.palette.error.dark}>{trackInfo.fallbackArtist}</Typography>
         </TitleArtistPlayerBox>
       </Box>
-    </Tooltip>
+    </CustomWidthTooltip>
   );
 }
